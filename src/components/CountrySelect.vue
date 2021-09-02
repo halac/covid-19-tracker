970cc7cb3d8b3e1fdef7c07049cfc8b7dda27358
halac <halac122@gmail.com>
2021-09-02 16:46:42 +0300

    Covid-19 Tracker

---
+++ src/components/CountrySelect.vue
@@ -0,0 +1,23 @@
+<template>
+  <select v-model="selected" class="form-select mt-10 block w-full border p-3 rounded">
+      <option value="0">Select Country</option>
+      <option v-for="country in countries" :value="country.ID"></option>
+
+  </select>
+</template>
+
+<script>
+export default {
+    name: 'CountrySelect',
+    props: ['Countries'],
+    data() {
+        return {
+            Selected: 0
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
